03dc863ae5bbc2169149070519e755ff05ae8864
Carl Mäsak <carl@masak.org>
2026-04-07 19:49:50 +0800

    Add second half of exercise 1.1.i.

--- src/exercise-1-1-i.lean
+++ src/exercise-1-1-i.lean
@@ -18,3 +18,11 @@ theorem iso_inv_unique {X Y : C} {f : X → Y} {g h : Y → X}
     _ = h ≫ (f ≫ g)   := by simp
     _ = h ≫ 𝟙 X       := by rw [fg]
     _ = h
+
+/-- If f has a left inverse g and a right inverse h,
+    then f is an isomorphism. -/
+theorem section_retraction_isIso {X Y : C} {f : X ⟶ Y} {g h : Y ⟶ X}
+    (fg : f ≫ g = 𝟙 X) (hf : h ≫ f = 𝟙 Y) :
+    IsIso f := by
+  have gh : g = h := iso_inv_unique hf fg
+  exact ⟨g, fg, by rw [gh]; exact hf⟩
