6ef73d243ccd424f092203fb8192daddf41485d1
Carl Mäsak <carl@masak.org>
2026-04-07 19:59:22 +0800

    Make proofs look neater and cleaner

--- src/exercise-1-1-i.lean
+++ src/exercise-1-1-i.lean
@@ -10,7 +10,7 @@ variable {C : Type u} [Category.{v} C]
 /-- A morphism has at most one two-sided inverse.
     If g and h are both left and right inverses of f, then g = h. -/
 theorem iso_inv_unique {X Y : C} {f : X → Y} {g h : Y → X}
-    (hf : h ≫ f = 𝟙 Y) (fg : f ≫ g = 𝟙 X)
+    (fg : f ≫ g = 𝟙 X) (hf : h ≫ f = 𝟙 Y)
     g = h := by
   calc
     g = 𝟙 Y ≫ g       := by simp
@@ -24,5 +24,8 @@ theorem iso_inv_unique {X Y : C} {f : X → Y} {g h : Y → X}
 theorem section_retraction_isIso {X Y : C} {f : X ⟶ Y} {g h : Y ⟶ X}
     (fg : f ≫ g = 𝟙 X) (hf : h ≫ f = 𝟙 Y) :
     IsIso f := by
-  have gh : g = h := iso_inv_unique hf fg
-  exact ⟨g, fg, by rw [gh]; exact hf⟩
+  have gh : g = h := iso_inv_unique fg hf
+  have gf : g ≫ f = 𝟙 Y := by 
+    rw [gh]
+    exact hf
+  exact ⟨g, fg, gf⟩
