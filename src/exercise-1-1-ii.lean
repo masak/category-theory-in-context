0ed873395e429a92c7dbb2763102a7d2f64322dd
Carl Mäsak <carl@masak.org>
2026-04-09 13:30:56 +0800

    Add exercise-1-1-ii.lean
    Without the proofs filled in for now.

---
+++ src/exercise-1-1-ii.lean
@@ -0,0 +1,21 @@
+import Mathlib.CategoryTheory.Groupoid
+import Mathlib.CategoryTheory.Core
+
+open CategoryTheory
+
+universe u v
+
+variable {C : Type u} [Category.{v} C]
+
+/-- Exercise 1.1.ii. Let C be a category. Show that the collection of
+    isomorphisms in C defines a subcategory, the **maximal groupoid**
+    inside C. --/
+theorem core_is_subcategory : (Core.inclusion C).Faithful := by
+  sorry
+
+theorem core_is_groupoid : Groupoid.{v} (Core C) := by
+  sorry
+
+theorem core_is_maximal {G : Type u} [Groupoid.{v} G] (F : G ⥤ C) :
+    ∃ (F' : G ⥤ Core C), F' ⋙ Core.inclusion C = F := by
+  sorry
