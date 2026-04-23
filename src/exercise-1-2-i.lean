d3e1ec3d108cf122764056a989d6d25efddbd2c9
Carl Mäsak <carl@masak.org>
2026-04-23 13:28:15 +0800

    Change to dot notation

--- src/exercise-1-2-i.lean
+++ src/exercise-1-2-i.lean
@@ -11,7 +11,7 @@ variable {C : Type u} [Category.{v} C] (c : C)
 /-- Exercise 1.2.i. Show that C/c ≅ (c/(Cᵒᵖ))ᵒᵖ. Defining C/c to be
     (c/(Cᵒᵖ))ᵒᵖ, deduce Exercise 1.1.iii(ii) from Exercise 1.1.iii(i).
     -/
-def OverUnderOpEquiv : Over c ≌ (Under (op C))ᵒᵖ :=
+def OverUnderOpEquiv : Over c ≌ (Under c.op)ᵒᵖ :=
   Comma.opEquiv (𝟙 C) (Functor.fromPUnit c)
 
 -- Make sure that (c/(Cᵒᵖ))ᵒᵖ is a Category.
