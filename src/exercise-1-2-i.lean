ccb84df89f700c606b001ac437bd41f492742506
Carl Mäsak <carl@masak.org>
2026-04-23 13:15:44 +0800

    Add check that (c/(Cᵒᵖ))ᵒᵖ is a category

--- src/exercise-1-2-i.lean
+++ src/exercise-1-2-i.lean
@@ -10,6 +10,8 @@ variable {C : Type u} [Category.{v} C] (c : C)
 /-- Exercise 1.2.i. Show that C/c ≅ (c/(Cᵒᵖ))ᵒᵖ. Defining C/c to be
     (c/(Cᵒᵖ))ᵒᵖ, deduce Exercise 1.1.iii(ii) from Exercise 1.1.iii(i).
     -/
-
 def OverUnderOpEquiv : Over c ≌ (Under (op C))ᵒᵖ :=
   Comma.opEquiv (𝟙 C) (Functor.fromPUnit c)
+
+-- Make sure that (c/(Cᵒᵖ))ᵒᵖ is a Category.
+#synth Category ((Under (op c))ᵒᵖ)
