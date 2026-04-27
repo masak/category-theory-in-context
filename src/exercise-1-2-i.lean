31aac08a4620bcc663523d7908bbce8840fbcaef
Carl Mäsak <carl@masak.org>
2026-04-27 13:24:45 +0800

    Fix faulty/inappropriate syntax

--- src/exercise-1-2-i.lean
+++ src/exercise-1-2-i.lean
@@ -1,7 +1,7 @@
 import Mathlib.CategoryTheory.Comma.Over.Basic
 import Mathlib.CategoryTheory.Opposites
 
-open CategoryTheory
+open CategoryTheory Opposite
 
 universe v u
 
@@ -10,7 +10,7 @@ variable {C : Type u} [Category.{v} C] (c : C)
 /-- Exercise 1.2.i. Show that C/c ≅ (c/(Cᵒᵖ))ᵒᵖ. Defining C/c to be
     (c/(Cᵒᵖ))ᵒᵖ, deduce Exercise 1.1.iii(ii) from Exercise 1.1.iii(i).
     -/
-def OverUnderOpEquiv : Over c ≌ (Under c.op)ᵒᵖ :=
+def OverUnderOpEquiv : Over c ≌ (Under (op c))ᵒᵖ :=
   Comma.opEquiv (𝟙 C) (Functor.fromPUnit c)
 
 -- Make sure that (c/(Cᵒᵖ))ᵒᵖ is a Category.
